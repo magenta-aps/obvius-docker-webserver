cded9ffa23346abe19085227271efee8dd04a954
Mads Damgaard Pedersen <mdp@magenta-aps.dk>
2018-11-14 16:10:25 +0100

    Added script for ignored directories, added entrypoint to wait for db, expanded templating in sql file

--- web/add_ignored.sh
+++ web/add_ignored.sh
@@ -1,13 +1,22 @@
-mkdir -p volumes/www.ku.dk/docs/upload
-mkdir -p volumes/www.ku.dk/var 
-mkdir -p volumes/www.ku.dk/logs 
-mkdir -p volumes/www.ku.dk/var/document_cache
+cd volumes
 
-ln -s volumes/www.ku.dk/var/document_cache volumes/www.ku.dk/docs/cache
+mkdir -p www.ku.dk/docs/upload
+mkdir -p www.ku.dk/var 
+mkdir -p www.ku.dk/logs 
+mkdir -p www.ku.dk/var/document_cache
+
+cd www.ku.dk/docs
+ln -s ../var/document_cache cache
+cd ../../
 
 #Add ignored conf files
-cp ./conf_files/hostmap.conf volumes/www.ku.dk/conf/hostmap.conf
-cp ./conf_files/tripleDES.txt volumes/www.ku.dk/conf/tripleDES.txt
-cp ./conf_files/names.conf volumes/www.ku.dk/conf/names.conf
-cp ./conf_files/https_hostmap.conf volumes/www.ku.dk/var/https_hostmap.conf
+cp ../conf_files/hostmap.conf www.ku.dk/conf/hostmap.conf
+cp ../conf_files/tripleDES.txt www.ku.dk/conf/tripleDES.txt
+cp ../conf_files/names.conf www.ku.dk/conf/names.conf
+cp ../conf_files/https_hostmap.conf www.ku.dk/var/https_hostmap.conf
 
+
+chown -R www-data www.ku.dk/docs/upload
+chown www-data www.ku.dk/var/
+chown -R www-data www.ku.dk/logs
+chown -R www-data www.ku.dk/var/document_cache

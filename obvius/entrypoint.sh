7d61634269e96e76111db32caf74279f2aae9452
Emil Thorenfeldt <emt@magenta-aps.dk>
2018-12-06 16:58:48 +0100

    Cleanup entrypoint and add PERL5LIB env

--- obvius/entrypoint.sh
+++ obvius/entrypoint.sh
@@ -1,9 +1,1 @@
 echo 'Entering entrypoint.sh'
-
-echo 'Building obvius translations'
-
-perl /var/www/obvius/bin/obvius_translations.pl sort_translations ku
-perl /var/www/obvius/bin/obvius_translations.pl build ku
-
-touch /var/www/dummyfile
-tail -f /var/www/dummyfile

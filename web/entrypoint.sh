16d2fee374a242c2153023b31dc8473e82ca14d7
Jørgen Ulrik B. Krag <jubk@magenta.dk>
2019-07-05 17:01:27 +0200

    New rhel-based web container

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -2,18 +2,42 @@
 
 echo 'Entering entrypoint.sh'
 
-echo 'Modifying apache user'
-APACHE_USER_ID="${OBVIUS_CONFIG_APACHE_USER_ID:-1000}"
-APACHE_USER_NAME="${OBVIUS_CONFIG_APACHE_USER_NAME:-"www-data"}"
-usermod -u $APACHE_USER_ID $APACHE_USER_NAME
+if [[ -z "${OBVIUS_LOGNAME}" ]]; then
+    OBVIUS_LOGNAME=`hostname`
+    if [[ -z "${OBVIUS_LOGNAME}" ]]; then
+        OBVIUS_LOGNAME="default"
+    fi
+    export OBVIUS_LOGNAME
+fi
+
+if [[ -z "${APACHE_SERVER_ADMIN}" ]]; then
+    APACHE_SERVER_ADMIN="KOM-support@adm.ku.dk"
+    export APACHE_SERVER_ADMIN
+fi
 
 echo 'Building obvius translations'
 
 perl /var/www/obvius/bin/obvius_translations.pl sort_translations ku
 perl /var/www/obvius/bin/obvius_translations.pl build ku
 
-echo "Waiting ${WAIT_FOR_DB_TIMER} seconds for database to spin up"
-sleep ${WAIT_FOR_DB_TIMER}
+if [[ -z "${WAIT_FOR_DB_TIMER}" ]]; then
+    WAIT_FOR_DB_TIMER=10
+fi
+
+echo -n "Waiting up to ${WAIT_FOR_DB_TIMER} seconds for database to spin up: "
+for x in $(seq 1 ${WAIT_FOR_DB_TIMER}); do
+    echo -n "$x "
+    perl -MObvius::Config -MDBI -e '
+        my $c = Obvius::Config->new("ku");
+        DBI->connect(
+            $c->param("dsn"),
+            $c->param("normal_db_login"),
+            $c->param("normal_db_passwd"),
+        ) or die "connection failed";
+    ' 2>/dev/null && break;
+    sleep 1
+done
+echo "... done!"
 
 echo "Updating root domain"
 # Handle updates to hostnames if defined in environment variables
@@ -23,10 +47,6 @@ if [[ "x${OBVIUS_ROOT_DOMAIN}" != "x" ]]; then
         # Replace hostnames in Obvius config
         sed -i "s/cms.ku.localhost.magenta.dk/${OBVIUS_HTTP_PREFIX}${OBVIUS_ROOT_DOMAIN}/g" "/etc/obvius/ku.conf"
         sed -i "s/cms.ku-ssl.localhost.magenta.dk/${OBVIUS_HTTPS_PREFIX}${OBVIUS_ROOT_DOMAIN}/g" "/etc/obvius/ku.conf"
-
-        # Replace hostnames in apache setup
-        sed -i "s/cms.ku.localhost.magenta.dk/${OBVIUS_HTTP_PREFIX}${OBVIUS_ROOT_DOMAIN}/g" "/etc/apache2/sites-available/ku.conf"
-        sed -i "s/cms.ku-ssl.localhost.magenta.dk/${OBVIUS_HTTPS_PREFIX}${OBVIUS_ROOT_DOMAIN}/g" "/etc/apache2/sites-available/ku.conf"
 
         perl -MObvius -MObvius::Config -e '
             Obvius->new(Obvius::Config->new("ku"))->dbh->do(qq|
@@ -44,11 +64,49 @@ if [[ "x${OBVIUS_ROOT_DOMAIN}" != "x" ]]; then
     fi
 fi
 
+# Once OBVIUS_ROOT_DOMAIN has been checked, set a default that can be used
+# in apache configurations
+if [[ -z "${OBVIUS_ROOT_DOMAIN}" ]]; then
+    OBVIUS_ROOT_DOMAIN="localhost.magenta.dk"
+    export OBVIUS_ROOT_DOMAIN
+fi
+
 # TODO: This needs to be replaced with automatic updating from checking a timestamp file
 # Rebuild hostmap files
 perl /var/www/www.ku.dk/bin/update_subsite_files.pl
 
-bash /var/conf_files/add_ignored.sh
+if [[ ! -e /var/www/www.ku.dk/var/document_cache ]]; then
+    mkdir -pv /var/www/www.ku.dk/var/document_cache
+fi
 
-echo 'Starting Apache'
-apachectl -D FOREGROUND
+if [[ ! -e /var/www/www.ku.dk/docs/cache ]]; then
+    ln -s ../var/document_cache /var/www/www.ku.dk/docs/cache
+fi
+
+#Add ignored conf files
+if [[ ! -e /var/www/www.ku.dk/conf/tripleDES.txt ]]; then
+    cp /var/conf_files/tripleDES.txt /var/www/www.ku.dk/conf/tripleDES.txt
+fi
+
+# Make sure log directory exists
+if [[ ! -d "/var/www/www.ku.dk/logs/${OBVIUS_LOGNAME}" ]]; then
+    mkdir -p "/var/www/www.ku.dk/logs/${OBVIUS_LOGNAME}"
+fi
+
+# Copy some default files
+for x in \
+    httpd.conf \
+    preload_obvius_modules.pl \
+; do
+    if [[ ! -e "/var/www/www.ku.dk/local/conf/${x}" ]]; then
+        cp "/var/conf_files/${x}" "/var/www/www.ku.dk/local/conf/${x}"
+    fi
+done
+
+# Make sure apache configuration is symlinked to file in www.ku.dk/local
+if [[ ! -h /opt/rh/httpd24/root/etc/httpd/conf/httpd.conf ]]; then
+    mv /opt/rh/httpd24/root/etc/httpd/conf/httpd.conf /opt/rh/httpd24/root/etc/httpd/conf/httpd.conf.orig
+    ln -s /var/www/www.ku.dk/local/conf/httpd.conf /opt/rh/httpd24/root/etc/httpd/conf/httpd.conf
+fi
+
+exec "$@"
